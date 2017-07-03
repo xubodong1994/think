3a9ba8c4901a7ab0916d7e3ef46063835f4a12d8
xubodong1994 <xubd1137357896@gmail.com>
2017-07-03 17:40:17 +0800

    verfy

--- thinkphp/library/think/log/driver/File.php
+++ thinkphp/library/think/log/driver/File.php
@@ -43,7 +43,6 @@ class File
         $destination = $this->config['path'] . date('Ym') . DS . date('d') . '.log';
 
         $path = dirname($destination);
-		echo $path;
         !is_dir($path) && mkdir($path, 0755, true);
 
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
