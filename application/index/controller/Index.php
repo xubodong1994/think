7d4b6b1798bc7959b3e649d7cd2153c322dc4576
xubodong1994 <xubd1137357896@gmail.com>
2017-07-05 18:36:16 +0800

    work

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -136,4 +136,15 @@ class Index
 			echo $info;
 		}
 	}
+
+	function http_curl() {
+		$ch = curl_init();
+		$url = 'http://www.baidu.com';
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$output = curl_exec($ch);
+		curl_close($ch);
+		var_dump($output);
+	}
 }
